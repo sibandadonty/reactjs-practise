bf785131176561ae71532ce39660654e618fed14
profsibz3090 <sibandadonty@gmail.com>
2025-01-16 10:12:35 +0200

    complete navbar with basic styling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div>Hello</div>;
+  return <NavBar />;
 }
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NavBar() {
+  const [nav, setNav] = useState(false);
+
   return (
-    <nav>
+    <nav className="flex justify-between px-8 py-4 bg-green-500 text-white">
       <h3>Brand Name</h3>
 
       {/* Desktop Navigation */}
 
-      <ul>
+      <ul className="hidden md:flex gap-2">
         <li>
           <a href="#">Services</a>
         </li>
@@ -22,11 +24,23 @@ function NavBar() {
         </li>
       </ul>
 
-      <p>Menu</p>
+      <p className="md:hidden" onClick={() => setNav(!nav)}>
+        Menu
+      </p>
 
       {/* Mobile Navigation */}
 
-      <ul>
+      <ul
+        className={`${
+          nav
+            ? "md:hidden absolute top-0 left-0 bottom-0 w-[65%] bg-green-500 p-2 ease-in-out duration-500"
+            : "md:hidden absolute top-0 left-[-1000px] bottom-0 w-[65%] bg-green-500 p-2 ease-in-out duration-500"
+        }`}
+      >
+        <li className="flex justify-between">
+          <a href="#">Brand Name</a>
+          <p onClick={() => setNav(false)} className="font-bold">X</p>
+        </li>
         <li>
           <a href="#">Services</a>
         </li>
