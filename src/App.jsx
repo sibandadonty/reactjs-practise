a79059067439ade6deb6898c734d027921a57fcf
profsibz3090 <sibandadonty@gmail.com>
2025-02-03 12:50:12 +0200

    routing using legacy react router dom methods <6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import A from "./A";
+import B from "./B";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<A />} />
+        <Route path="/b" element={<B />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/B.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function B() {
+  return (
+    <div>
+      Compoenent B
+    </div>
+  )
+}
+
+export default B
