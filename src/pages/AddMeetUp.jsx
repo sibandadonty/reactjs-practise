b16f79552db3682ed07ce224de83e9faa720e60c
profsibz3090 <sibandadonty@gmail.com>
2025-02-04 14:44:00 +0200

    get all meetups from database added

--- src/pages/AddMeetUp.jsx
+++ src/pages/AddMeetUp.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import AddMeetUpForm from "../components/AddMeetUpForm";
 
 function AddMeetUp() {
--- src/pages/AllMeetUps.jsx
+++ src/pages/AllMeetUps.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function AllMeetUps() {
+  const [allMeetups, setAllMeetups] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(import.meta.env.VITE_DATABASE_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        
+      });
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading) {
+    return <div className="min-h-screen w-full flex items-center justify-center">
+      <h1 className="text-2xl">Loading</h1>
+    </div>
+  }
+
   return (
     <div>
       <h1>All Meet Ups</h1>
-      
     </div>
-  )
+  );
 }
 
-export default AllMeetUps
+export default AllMeetUps;
