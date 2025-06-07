7b1846b2f2b35d3078cca315020259ec885d942c
profsibz3090 <sibandadonty@gmail.com>
2025-06-07 10:35:16 +0200

    back to the basics fresh start

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import AddMeetUp from "./pages/AddMeetUp";
-import AllMeetUps from "./pages/AllMeetUps";
-import FavouriteMeetUps from "./pages/FavouriteMeetUps";
-import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<AllMeetUps />} />
-          <Route path="/add" element={<AddMeetUp />} />
-          <Route path="/favorites" element={<FavouriteMeetUps />} />
-        </Routes>
-      </BrowserRouter>
+      <h1>Hello World</h1>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import { FavoritesContextProvider } from "./store/favorite-context";
 
 ReactDOM.render(
-  <FavoritesContextProvider>
-    <App />
-  </FavoritesContextProvider>,
+    <App />,
   document.getElementById("root")
 );
