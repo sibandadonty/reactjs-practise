7fd6bad807e441267ee24d3e0078acf2c91725d4
profsibz3090 <sibandadonty@gmail.com>
2025-06-10 16:20:30 +0200

    added styling for the hero section

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,15 +4,15 @@ import ProductsCard from "./ProductsCard";
 
 function MainContent() {
   return (
-    <div className="px-4">
+    <div className="px-4 flex flex-col gap-4 items-start">
       <img src="./image-web-3-mobile.jpg" alt="web 3 mobile" />
       <p className="font-bold text-4xl">The Bright Future of Web 3.0?</p>
-      <p>
+      <p className="leading-8 text-xl">
         We dive into the next evolution of the web that claims to put the power
         of the platforms back into the hands of the people. But is it really
         fulfilling its promise?
       </p>
-      <button>READ MORE</button>
+      <button className="bg-red-500 font-bold tracking-wider px-4 py-2">READ MORE</button>
       <NewCards />
       <ProductsCard />
     </div>
